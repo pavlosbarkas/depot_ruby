a4e29c8e62fa07a46b5f62e6dafa3f32e0ba4368
pavlosbarkas <pavlosbarkas@outlook.com>
2022-02-16 12:43:46 +0200

    capturing and creating order and feed them through an atom feed

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,10 @@
 class LineItem < ApplicationRecord
+  belongs_to :order, optional: true # optional: true gives the ability for line_items
+                                    # to be created before added to an order.
+                                    # if we didn't set optional:true we would get error
+                                    # when a line_item was created without being part of an order
   belongs_to :product
-  belongs_to :cart
+  belongs_to :cart, optional: true
 
   def total_price
     product.price * quantity
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+  # dependent: :destroy specifies that when an order is destroyed
+  # all its line items will also be destroyed
+
   enum pay_type: {
     "Check"          => 0,
     "Credit card"    => 1,
@@ -7,4 +11,12 @@ class Order < ApplicationRecord
 
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
+
 end
