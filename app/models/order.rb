3608c8cbdb540d1c3273cb20626068e7ba3f123b
pavlosbarkas <pavlosbarkas@outlook.com>
2022-02-21 16:11:46 +0200

    setup payment as active job

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
+require 'pago'
+
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
   # dependent: :destroy specifies that when an order is destroyed
@@ -21,6 +23,40 @@ validates :pay_type, inclusion: pay_types.keys
       item.cart_id = nil
       line_items << item
     end
+  end
+
+  def charge!(pay_type_params)
+    payment_details = {}
+    payment_method = nil
+
+    case pay_type
+    when 'Check'
+      payment_method = :check
+      payment_details[:routing] = pay_type_params[:routing_number]
+      payment_details[:account] = pay_type_params[:account_number]
+    when 'Credit Card'
+      payment_method = :credit_card
+      payment_details[:cc_num] = pay_type_params[:credit_card_number]
+      month, year = pay_type_params[:expiration_date].split(//)
+      payment_details[:expiration_month] = month
+      payment_details[:expiration_year] = year
+    when 'Purchase Order'
+      payment_method = :po
+      payment_details[:po_num] = pay_type_params[:po_number]
+    end
+
+    payment_result = Pago.make_payment(
+      order_id: id,
+      payment_method: payment_method,
+      payment_details: payment_details
+    )
+
+    if payment_result.succeeded?
+      OrderMailer.received(self).deliver_later
+    else
+      raise payment_result.error
+    end
+
   end
 
 end
