5e3ecaf67329a058db49b730ec09e479ac769f46
pavlosbarkas <pavlosbarkas@outlook.com>
2022-03-03 12:07:45 +0200

    changed es locale currency format and added currency conversion based on locale

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,23 +1,32 @@
 class Product < ApplicationRecord
-    has_many :line_items
-    has_many :orders, through: :line_items
-    before_destroy :ensure_not_referenced_by_any_line_item
+  has_many :line_items
+  has_many :orders, through: :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
-    private
-        # ensure that there are no line items referencing this product
-        def ensure_not_referenced_by_any_line_item
-            unless line_items.empty?
-                errors.add(:base, 'Line Items present')
-                throw :abort
-            end
-        end
+  def locale_price
+      if I18n.locale.to_s == 'en'
+          price
+      elsif I18n.locale.to_s == 'es'
+          price * 0.9
+      end
+  end
 
-    validates :title, :description, :image_url, presence: true
-    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-    validates :title, uniqueness: true
-    validates :image_url, allow_blank: true, format: {
-        with: %r{\.(gif|jpg|png)\z}i, #i ignores case , \z the end of the input
-        message: 'must be a URL for GIF, JPG or PNG image.'
-    }
-    validates :title, length: {minimum: 10, message: 'must be at least 10 characters long.'}
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i, # i ignores case , \z the end of the input
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :title, length: {minimum: 10, message: 'must be at least 10 characters long.'}
 end
